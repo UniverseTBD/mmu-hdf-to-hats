5fb00ebaa1620c0dedcc5b51e5f5854057bb9145
Tobias Pitters <tobias.pitters@gmail.com>
2025-12-18 11:54:59 +0100

    fix sdss comparison

--- verification/download_sdss.sh
+++ verification/download_sdss.sh
@@ -1,4 +1,5 @@
 # execute from mmu-to-hats directory
+cp catalog_download_scripts/sdss.py data/MultimodalUniverse/v1/sdss/sdss.py
 wget -r -np -nH --cut-dirs=1 -R "index.html*" -q https://users.flatironinstitute.org/~polymathic/data/MultimodalUniverse/v1/sdss/sdss/healpix=583/
 # For some reason this file is access restricted, other <catalog>.py files should work though
 # wget -r -np -nH --cut-dirs=1 -R "index.html*" -q https://users.flatironinstitute.org/~polymathic/data/MultimodalUniverse/v1/sdss/sdss.py
