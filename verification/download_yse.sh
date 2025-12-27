06524c22dca6597630eaaf8109b218c1fc3362c2
Tobias Pitters <31857876+CloseChoice@users.noreply.github.com>
2025-12-27 12:22:09 +0100

    Add yse (#52)
    * WIP: len of unsized object type error
    
    * fix yse comparison
    
    * add catalog to workflow

---
+++ verification/download_yse.sh
@@ -0,0 +1,4 @@
+# execute from mmu-to-hats directory
+wget -r -np -nH --cut-dirs=1 -R "index.html*" -q https://users.flatironinstitute.org/~polymathic/data/MultimodalUniverse/v1/yse/yse_dr1/healpix=0584/
+# For some reason this file is access restricted, other <catalog>.py files should work though
+wget -r -np -nH --cut-dirs=1 -R "index.html*" -q https://users.flatironinstitute.org/~polymathic/data/MultimodalUniverse/v1/yse/yse.py
